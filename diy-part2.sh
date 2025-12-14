73082d34fab6aba1173f3bb9f2d1739a5bf62020
yd2005 <76555598+yd2005@users.noreply.github.com>
2025-12-14 22:34:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,65 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+# =================================================================
+# Function: Smart Glider Injection (Auto-detect Arch)
+# =================================================================
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+mkdir -p files/usr/bin
+mkdir -p files/etc/init.d
+mkdir -p files/etc/uci-defaults
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# --- 1. 智能判断架构 ---
+# 检查 .config 文件中是否包含 x86_64 的定义
+if grep -q "CONFIG_TARGET_x86_64=y" .config; then
+    echo "Detected Architecture: x86_64 (AMD64)"
+    GLIDER_ARCH="amd64"
+else
+    # 默认为 ARM64 (适配 AX6)
+    echo "Detected Architecture: ARM64"
+    GLIDER_ARCH="arm64"
+fi
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# --- 2. 下载对应版本的 Glider ---
+GLIDER_VER="v0.16.3"
+GLIDER_URL="https://github.com/nadoo/glider/releases/download/${GLIDER_VER}/glider_${GLIDER_VER##v}_linux_${GLIDER_ARCH}.tar.gz"
+
+echo "Downloading Glider from: $GLIDER_URL"
+wget -O glider.tar.gz "$GLIDER_URL"
+
+# 解压并提取
+tar -xzvf glider.tar.gz
+find . -type f -name "glider" -exec mv {} files/usr/bin/glider \;
+rm -rf glider.tar.gz glider_*
+chmod +x files/usr/bin/glider
+
+# --- 3. 生成启动脚本 (Procd) ---
+# 注意：启动脚本是通用的，不需要改动
+cat <<EOF > files/etc/init.d/glider
+#!/bin/sh /etc/rc.common
+
+START=99
+USE_PROCD=1
+PROG=/usr/bin/glider
+
+# 监听 8443，连接本地 1001/1002/1003
+ARGS="-listen mixed://:8443 -forward socks5://127.0.0.1:1001 -forward socks5://127.0.0.1:1002 -forward socks5://127.0.0.1:1003 -strategy ha -check=http://www.gstatic.com/generate_204 -checkinterval=30"
+
+start_service() {
+    procd_open_instance
+    procd_set_param command \$PROG \$ARGS
+    procd_set_param respawn
+    procd_set_param stdout 0
+    procd_set_param stderr 0
+    procd_close_instance
+}
+EOF
+chmod +x files/etc/init.d/glider
+
+# --- 4. 设置自动启动 ---
+cat <<EOF > files/etc/uci-defaults/99-glider-enable
+#!/bin/sh
+/etc/init.d/glider enable
+exit 0
+EOF
+chmod +x files/etc/uci-defaults/99-glider-enable
+
+echo "=== Glider Injection ($GLIDER_ARCH) Done ==="
